39eee593d35225260910cd86e69f830b134006fc
Oleksandr Berchenko <ar.ukraine@gmail.com>
2020-07-13 16:13:26 +0300

    Update GIMP plugins

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+**Setup**
+
 1. Clone this repo to a local folder.
 
 2. Add the folder with the cropped artwork to your own Google Drive to be able to sync it locally.
@@ -18,59 +20,58 @@ versions from this repo.
 
 6. Install GIMP (https://www.gimp.org/downloads/).
 
-7. Make sure that macros are enabled in Microsoft Excel.
+7. Open GIMP, go to `Edit` -> `Preferences` -> `Folders` -> `Plug-ins`, add `gimp` folder
+from this repo, click `OK` and then close GIMP.
 
-8. Go to the repo folder.  Either install Anaconda and use it to open a JupyterHub or use VirtualEnv:
+8. Make sure that macros are enabled in Microsoft Excel.
+
+9. Go to the repo folder.  Either install Anaconda and use it to open a JupyterHub or use VirtualEnv:
 
   - Install Python 3.7 (or other Python 3 version), Pip and VirtualEnv.
   - `virtualenv env --python=python3.7`
   - `.\env\Scripts\activate.bat` (Windows) or `source env/bin/activate` (Mac)
-  - `pip install jupyter pyyaml requests xlwings`
+  - `pip install jupyter pylint pyyaml requests xlwings`
   - `jupyter notebook`
 
-9. Edit `configuration.yaml`:
+10. Edit `configuration.yaml`:
 
   - Set `sheet_gdid` (Google Drive ID of the cards spreadsheet).
   - If needed, update `sheet_type` (either `xlsm` or `xlsx`).
   - Set `artwork_path` (local path to the folder with the cropped artwork).
 
-10. Open `setGenerator.ipynb` and follow further instructions.
+11. Open `setGenerator.ipynb` and follow further instructions.
 
 
 **GIMP Plugins**
 
-`prepare_makeplayingcards.py`: two plugins to prepare images for MakePlayingCards printing (one for a single image,
-another for a folder).  At the moment they make 3 things:
+You may use GIMP plugins separately.  See description of each of them in `gimp/scripts.py`.
 
-1. rotate landscape images (if image name ends with `-Back-Face` or `-2` then clockwise, otherwise counter-clockwise);
-2. crop bleed margins to satisfy MakePlayingCards requirements;
-3. export to PNG with the maximum compression level (still lossless, just saves some space).
+Setup:
 
-They support images in 300, 600 and 800 dpi images (original resolution is preserved).
-
-Installation:
-
-1. Open GIMP
-2. Edit -> Preferences -> Folders -> Plug-ins
-3. Add `gimp` folder from this repo
-4. Restart GIMP
+1. Install GIMP (https://www.gimp.org/downloads/).
+2. Open GIMP, go to `Edit` -> `Preferences` -> `Folders` -> `Plug-ins`, add `gimp` folder
+from this repo, click `OK` and then restart GIMP.
 
 Usage:
 
 For a single image:
 
-1. Open image file in GIMP
-2. Filters -> Prepare MakePlayingCards
+1. Open the image file in GIMP
+2. Filters -> Script Name
 3. Specify Output folder
 
 For a folder:
 
-1. Open GIMP
-2. Filters -> Prepare MakePlayingCards Folder
+1. Open any image file in GIMP
+2. Filters -> Script Name (with "Folder" at the end)
 3. Specify Input folder and Output folder
 
-or from CLI:
+From CLI:
+
+`gimp-console-2.10 -i -b "(python-<script name with dashes>-folder 1 \"<path to input folder>\" \"path to output folder\")"`
+
+For example:
 
 `gimp-console-2.10 -i -b "(python-prepare-makeplayingcards-folder 1 \"Input\" \"Output\")"`
 
-GIMP executable on your environment may be slightly different.
+Please note that GIMP executable on your environment may be slightly different.
---
+++ gimp/scripts.py
@@ -0,0 +1,309 @@
+#!/usr/bin/env python
+""" Custom GIMP plugin(s).
+"""
+import os
+from gimpfu import (gimp, pdb, register, main, PF_IMAGE, PF_DRAWABLE,
+                    PF_DIRNAME, ROTATE_90, ROTATE_270)
+
+def _get_filename_backside(img):
+    """ Determine card side and output file name.
+    """
+    file_name = pdb.gimp_image_get_filename(img)
+    file_name = os.path.split(file_name)[-1]
+    file_name = '.'.join(file_name.split('.')[:-1])
+    back_side = file_name.endswith('-Back-Face') or file_name.endswith('-2')
+    file_name = file_name + '.png'
+    return (file_name, back_side)
+
+def _get_rotation(drawable):
+    """ Determine whether an image should be rotated or not.
+    """
+    rotation = ((drawable.width == 1050 and drawable.height == 750)
+                or (drawable.width == 1126 and drawable.height == 826)
+                or (drawable.width == 2100 and drawable.height == 1500)
+                or (drawable.width == 2250 and drawable.height == 1650)
+                or (drawable.width == 2800 and drawable.height == 2000)
+                or (drawable.width == 3000 and drawable.height == 2200))
+    return rotation
+
+def _get_mpc_clip_size(drawable):
+    """ Determine MakePlayingCards clip size.
+    """
+    if ((drawable.width == 826 and drawable.height == 1126)
+            or (drawable.width == 1126 and drawable.height == 826)):
+        size = 2
+    elif ((drawable.width == 1650 and drawable.height == 2250)
+          or (drawable.width == 2250 and drawable.height == 1650)):
+        size = 3
+    elif ((drawable.width == 2200 and drawable.height == 3000)
+          or (drawable.width == 3000 and drawable.height == 2200)):
+        size = 4
+    else:
+        size = 0
+
+    return size
+
+def _get_pdf_margin_size(drawable):
+    """ Determine PDF bleed margin size.
+    """
+    if ((drawable.width == 750 and drawable.height == 1050)
+            or (drawable.width == 1050 and drawable.height == 750)):
+        size = 38
+    elif ((drawable.width == 1500 and drawable.height == 2100)
+          or (drawable.width == 2100 and drawable.height == 1500)):
+        size = 75
+    elif ((drawable.width == 2000 and drawable.height == 2800)
+          or (drawable.width == 2800 and drawable.height == 2000)):
+        size = 100
+    else:
+        size = 0
+
+    return size
+
+def _rotate(drawable, back_side):
+    """ Rotate an image.
+    """
+    if back_side:
+        pdb.gimp_item_transform_rotate_simple(drawable, ROTATE_90, False,
+                                              drawable.height / 2,
+                                              drawable.height / 2)
+    else:
+        pdb.gimp_item_transform_rotate_simple(drawable, ROTATE_270, False,
+                                              drawable.width / 2,
+                                              drawable.width / 2)
+
+def _clip(img, drawable, size):
+    """ Clip an image.
+    """
+    if size:
+        new_width = drawable.width - 2 * size
+        new_height = drawable.height - 2 * size
+        pdb.gimp_image_resize(img, new_width, new_height, -size, -size)
+        pdb.gimp_layer_resize(drawable, new_width, new_height, -size, -size)
+
+def _add_margin(img, drawable, size):
+    """ Add bleed margin to an image.
+    """
+    if size:
+        new_width = drawable.width + 2 * size
+        new_height = drawable.height + 2 * size
+        pdb.gimp_image_resize(img, new_width, new_height, size, size)
+        pdb.gimp_layer_resize(drawable, new_width, new_height, size, size)
+
+def _iterate_folder(input_folder, output_folder, func):
+    """ Apply a given function to a folder of images.
+    """
+    for file_name in os.listdir(input_folder):
+        if not (file_name.endswith('.png') or file_name.endswith('.jpg')):
+            continue
+
+        img = pdb.gimp_file_load(os.path.join(input_folder, file_name),
+                                 file_name)
+        drawable = img.layers[0]
+        func(img, drawable, output_folder)
+
+def prepare_pdf_front(img, drawable, output_folder):
+    """ Prepare a front image for PDF document.
+    """
+    gimp.progress_init('Prepare a front image for PDF document...')
+    pdb.gimp_undo_push_group_start(img)
+
+    try:
+        file_name, back_side = _get_filename_backside(img)
+    except Exception:  # pylint: disable=W0703
+        pdb.gimp_undo_push_group_end(img)
+        return
+
+    if back_side:
+        pdb.gimp_undo_push_group_end(img)
+        return
+
+    rotation = _get_rotation(drawable)
+    margin_size = _get_pdf_margin_size(drawable)
+
+    if rotation:
+        _rotate(drawable, back_side)
+
+    _add_margin(img, drawable, margin_size)
+    pdb.file_png_save(img, drawable,
+                      os.path.join(output_folder, file_name), file_name,
+                      0, 9, 1, 0, 0, 1, 1)
+    pdb.gimp_undo_push_group_end(img)
+
+def prepare_pdf_back(img, drawable, output_folder):
+    """ Prepare a back image for PDF document.
+    """
+    gimp.progress_init('Prepare a back image for PDF document...')
+    pdb.gimp_undo_push_group_start(img)
+
+    try:
+        file_name, back_side = _get_filename_backside(img)
+    except Exception:  # pylint: disable=W0703
+        pdb.gimp_undo_push_group_end(img)
+        return
+
+    if not back_side:
+        pdb.gimp_undo_push_group_end(img)
+        return
+
+    rotation = _get_rotation(drawable)
+
+    if rotation:
+        _rotate(drawable, back_side)
+
+    pdb.file_png_save(img, drawable,
+                      os.path.join(output_folder, file_name), file_name,
+                      0, 9, 1, 0, 0, 1, 1)
+    pdb.gimp_undo_push_group_end(img)
+
+def prepare_makeplayingcards(img, drawable, output_folder):
+    """ Prepare an image for MakePlayingCards printing.
+    """
+    gimp.progress_init('Prepare an image for MakePlayingCards printing...')
+    pdb.gimp_undo_push_group_start(img)
+
+    try:
+        file_name, back_side = _get_filename_backside(img)
+    except Exception:  # pylint: disable=W0703
+        pdb.gimp_undo_push_group_end(img)
+        return
+
+    rotation = _get_rotation(drawable)
+    clip_size = _get_mpc_clip_size(drawable)
+
+    if rotation:
+        _rotate(drawable, back_side)
+
+    _clip(img, drawable, clip_size)
+    pdb.file_png_save(img, drawable,
+                      os.path.join(output_folder, file_name), file_name,
+                      0, 9, 1, 0, 0, 1, 1)
+    pdb.gimp_undo_push_group_end(img)
+
+def prepare_pdf_front_folder(input_folder, output_folder):
+    """ Prepare a folder of front images for PDF document.
+    """
+    gimp.progress_init(
+        'Prepare a folder of front images for PDF document...')
+    _iterate_folder(input_folder, output_folder, prepare_pdf_front)
+
+def prepare_pdf_back_folder(input_folder, output_folder):
+    """ Prepare a folder of back images for PDF document.
+    """
+    gimp.progress_init(
+        'Prepare a folder of back images for PDF document...')
+    _iterate_folder(input_folder, output_folder, prepare_pdf_back)
+
+def prepare_makeplayingcards_folder(input_folder, output_folder):
+    """ Prepare a folder of images for MakePlayingCards printing.
+    """
+    gimp.progress_init(
+        'Prepare a folder of images for MakePlayingCards printing...')
+    _iterate_folder(input_folder, output_folder, prepare_makeplayingcards)
+
+register(
+    'python_prepare_pdf_front',
+    'Prepare a front image for PDF document',
+    '1. Rotate a landscape image. 2. Add white bleed margins. 3. Export PNG.',
+    'A.R.',
+    'A.R.',
+    '2020',
+    'Prepare PDF Front',
+    '*',
+    [
+        (PF_IMAGE, 'image', 'Input image', None),
+        (PF_DRAWABLE, 'drawable', 'Input drawable', None),
+        (PF_DIRNAME, 'output_folder', 'Output folder', None)
+    ],
+    [],
+    prepare_pdf_front,
+    menu='<Image>/Filters')
+
+register(
+    'python_prepare_pdf_front_folder',
+    'Prepare a folder of front images for PDF document',
+    '1. Rotate a landscape image. 2. Add white bleed margins. 3. Export PNG.',
+    'A.R.',
+    'A.R.',
+    '2020',
+    'Prepare PDF Front Folder',
+    '*',
+    [
+        (PF_DIRNAME, 'input_folder', 'Input folder', None),
+        (PF_DIRNAME, 'output_folder', 'Output folder', None)
+    ],
+    [],
+    prepare_pdf_front_folder,
+    menu='<Image>/Filters')
+
+register(
+    'python_prepare_pdf_back',
+    'Prepare a back image for PDF document',
+    '1. Rotate a landscape image. 2. Export PNG.',
+    'A.R.',
+    'A.R.',
+    '2020',
+    'Prepare PDF Back',
+    '*',
+    [
+        (PF_IMAGE, 'image', 'Input image', None),
+        (PF_DRAWABLE, 'drawable', 'Input drawable', None),
+        (PF_DIRNAME, 'output_folder', 'Output folder', None)
+    ],
+    [],
+    prepare_pdf_back,
+    menu='<Image>/Filters')
+
+register(
+    'python_prepare_pdf_back_folder',
+    'Prepare a folder of back images for PDF document',
+    '1. Rotate a landscape image. 2. Export PNG.',
+    'A.R.',
+    'A.R.',
+    '2020',
+    'Prepare PDF Back Folder',
+    '*',
+    [
+        (PF_DIRNAME, 'input_folder', 'Input folder', None),
+        (PF_DIRNAME, 'output_folder', 'Output folder', None)
+    ],
+    [],
+    prepare_pdf_back_folder,
+    menu='<Image>/Filters')
+
+register(
+    'python_prepare_makeplayingcards',
+    'Prepare an image for MakePlayingCards printing',
+    '1. Rotate a landscape image. 2. Clip bleed margins. 3. Export PNG.',
+    'A.R.',
+    'A.R.',
+    '2020',
+    'Prepare MakePlayingCards',
+    '*',
+    [
+        (PF_IMAGE, 'image', 'Input image', None),
+        (PF_DRAWABLE, 'drawable', 'Input drawable', None),
+        (PF_DIRNAME, 'output_folder', 'Output folder', None)
+    ],
+    [],
+    prepare_makeplayingcards,
+    menu='<Image>/Filters')
+
+register(
+    'python_prepare_makeplayingcards_folder',
+    'Prepare a folder of images for MakePlayingCards printing',
+    '1. Rotate a landscape image. 2. Clip bleed margins. 3. Export PNG.',
+    'A.R.',
+    'A.R.',
+    '2020',
+    'Prepare MakePlayingCards Folder',
+    '*',
+    [
+        (PF_DIRNAME, 'input_folder', 'Input folder', None),
+        (PF_DIRNAME, 'output_folder', 'Output folder', None)
+    ],
+    [],
+    prepare_makeplayingcards_folder,
+    menu='<Image>/Filters')
+
+main()
