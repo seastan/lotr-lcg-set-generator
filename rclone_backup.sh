7bcd093768f7cd1530bcaee98d1c02a067cd8557
Oleksandr berchenko <ar.ukraine@gmail.com>
2021-03-07 19:00:10 +0200

    Create a cron to perform daily backups

---
+++ rclone_backup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Setup a cron as:
+# 7 0 * * *   /home/homeassistant/lotr-lcg-set-generator/rclone_backup.sh >> /home/homeassistant/.homeassistant/cron.log 2>&1
+DATE=$(date +%Y-%m-%d)
+rclone copy "/home/homeassistant/Drive/OCTGN Files/Set Folders" "ALeP:/Backup/$DATE/Set Folders"
+rclone copy "/home/homeassistant/Drive/OCTGN Files/Encounter Decks" "ALeP:/Backup/$DATE/Encounter Decks"
+rclone copy "/home/homeassistant/Drive/OCTGN Files/Scratch Set Folders" "ALeP:/Backup/$DATE/Scratch Set Folders"
+rclone copy "/home/homeassistant/Drive/OCTGN Files/Scratch Encounter Decks" "ALeP:/Backup/$DATE/Scratch Encounter Decks"
+echo "Done"
