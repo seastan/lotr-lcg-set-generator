3366745f15372627f2b8f5b28cb64f4cf1c5407d
Oleksandr berchenko <ar.ukraine@gmail.com>
2021-03-23 23:31:32 +0200

    Add script to monitor MPC links

--- mpc_monitor.py
+++ mpc_monitor.py
@@ -1,56 +1,52 @@
-# pylint: disable=W0703,C0301
-""" Cron for LotR ALeP workflow (Part 1, before Strange Eons).
-
-NOTE: This script heavily relies on my existing smart home environment.
-
-Create discord.yaml (see discord.default.yaml).
-
-Setup rclone:
-
-curl -L https://raw.github.com/pageauc/rclone4pi/master/rclone-install.sh | bash
-rclone confi
-
-Setup a cron as:
-*/2 * * * * flock -xn /home/homeassistant/lotr-lcg-set-generator/cron.lock -c 'python3 /home/homeassistant/lotr-lcg-set-generator/run_before_se_cron.py > /dev/null' 2>&1
+""" Monitor MakePlayingCards shared URLs.
 """
+# pylint: disable=W0703,C0301
 from datetime import datetime
 from email.header import Header
 import json
 import logging
 import os
 import re
-import subprocess
 import time
 import uuid
 
 import requests
 import yaml
 
-from lotr import SanityCheckError
-from run_before_se import main as imported_main
 
+COOKIES = {
+    "ASP.NET_SessionId": "m54mkkfuyyfl0df2wkfxhbp0",
+    "PrinterStudioCookie": "B13C6275B2336A094A58EE9CBF88F314ACDC4C729AA9F7C348727E005F44F3DEF92611CE25CF4082DD139E760A8531CEE24015DA7B3705BB1C994DD172C4888DDAE11891F5F980E933E2216DC5BAAC6B0B972B6A49E0D10E0BAC07D017F2E2685B45D901D850198E03BF4E900FBC6954926BD0D26F53F34F1C8A6A343090229BF64E6A973BDDFF70714030510F7E25D9B374DBE7C21443AFEC73FDE63EAE5EB1062C946072703C8DBE53FA75AD83F66D6A75868B3B884B3DAD4909E7FF1C2ED8C3C91392C682D8182437F8837710E26E5F577982",
+    "PrinterStudioUserName": "alongextendedparty@gmail.com"
+}
+
+ID_REGEX = r'src="\.\.\/\/PreviewFiles\/Normal\/temp\/thumb\/([0-9A-F]+)_'
+
+ALERT_SUBJECT_TEMPLATE = 'LotR MPC Monitor ALERT: {}'
+ERROR_SUBJECT_TEMPLATE = 'LotR MPC Monitor ERROR: {}'
+WARNING_SUBJECT_TEMPLATE = 'LotR MPC Monitor WARNING: {}'
+MAIL_QUOTA = 50
 
 DISCORD_CONF_PATH = 'discord.yaml'
 INTERNET_SENSOR_PATH = '/home/homeassistant/.homeassistant/internet_state'
-LOG_PATH = 'cron.log'
-MAIL_COUNTER_PATH = 'cron.cnt'
+LINKS_PATH = 'mpc_monitor.json'
+LOG_PATH = 'mpc_monitor.log'
+MAIL_COUNTER_PATH = 'mpc_monitor.cnt'
 MAILS_PATH = '/home/homeassistant/.homeassistant/mails'
-SANITY_CHECK_PATH = 'sanity_check.txt'
 WORKING_DIRECTORY = '/home/homeassistant/lotr-lcg-set-generator/'
 
-ERROR_SUBJECT_TEMPLATE = 'LotR ALeP Cron ERROR: {}'
-SANITY_CHECK_SUBJECT_TEMPLATE = 'LotR ALeP Cron CHECK: {}'
-WARNING_SUBJECT_TEMPLATE = 'LotR ALeP Cron WARNING: {}'
-MAIL_QUOTA = 50
+URL_TIMEOUT = 30
+URL_RETRIES = 3
+URL_SLEEP = 1
+
+
+class ResponseError(Exception):
+    """ Response error.
+    """
 
 
 class DiscordResponseError(Exception):
     """ Discord Response error.
-    """
-
-
-class RCloneError(Exception):
-    """ RClone error.
     """
 
 
@@ -73,53 +69,6 @@ def internet_state():
         logging.warning(message)
         create_mail(WARNING_SUBJECT_TEMPLATE.format(message))
         return True
-
-
-def get_sanity_check_message():
-    """ Get the latest sanity check message.
-    """
-    try:
-        with open(SANITY_CHECK_PATH, 'r') as obj:
-            return obj.read().strip()
-    except Exception:
-        return ''
-
-
-def set_sanity_check_message(message):
-    """ Set a new sanity check message.
-    """
-    try:
-        with open(SANITY_CHECK_PATH, 'w') as obj:
-            obj.write(message)
-    except Exception as exc:
-        message = str(exc)
-        logging.warning(message)
-        create_mail(WARNING_SUBJECT_TEMPLATE.format(message))
-
-
-def send_discord(message):
-    """ Send a message to a Discord channel.
-    """
-    try:
-        with open(DISCORD_CONF_PATH, 'r') as f_conf:
-            conf = yaml.safe_load(f_conf)
-
-        if conf.get('webhook_url'):
-            data = {'content': message}
-            res = requests.post(conf['webhook_url'], json=data)
-            res = res.content.decode('utf-8')
-            if res != '':
-                raise DiscordResponseError(
-                    'Non-empty response: {}'.format(res))
-
-            return True
-    except Exception as exc:
-        message = 'Discord message failed: {}: {}'.format(
-            type(exc).__name__, str(exc))
-        logging.exception(message)
-        create_mail(ERROR_SUBJECT_TEMPLATE.format(message))
-
-    return False
 
 
 def is_non_ascii(value):
@@ -210,62 +159,106 @@ def increment_mail_counter():
         pass
 
 
-def rclone():
-    """ Sync Google Drive.
+def send_discord(message):
+    """ Send a message to a Discord channel.
     """
-    res = subprocess.run('./rclone.sh', capture_output=True, shell=True,
-                         check=True)
-    stdout = res.stdout.decode('utf-8').strip()
-    stderr = res.stderr.decode('utf-8').strip()
-    logging.info('Rclone finished, stdout: %s, stderr: %s', stdout, stderr)
-    if stdout != 'Done':
-        raise RCloneError('RClone failed, stdout: {}, stderr: {}'.format(
-            stdout, stderr))
+    try:
+        with open(DISCORD_CONF_PATH, 'r') as f_conf:
+            conf = yaml.safe_load(f_conf)
 
+        if conf.get('webhook_url'):
+            data = {'content': message}
+            res = requests.post(conf['webhook_url'], json=data)
+            res = res.content.decode('utf-8')
+            if res != '':
+                raise DiscordResponseError(
+                    'Non-empty response: {}'.format(res))
+
+            return True
+    except Exception as exc:
+        message = 'Discord message failed: {}: {}'.format(
+            type(exc).__name__, str(exc))
+        logging.exception(message)
+        create_mail(ERROR_SUBJECT_TEMPLATE.format(message))
+
+    return False
+
+
+def get_url(session, url):
+    """ Get URL content.
+    """
+    for i in range(URL_RETRIES):
+        try:
+            req = session.get(url, timeout=URL_TIMEOUT)
+            res = req.content.decode('unicode-escape', errors='ignore')
+            break
+        except Exception:
+            if i < URL_RETRIES - 1:
+                time.sleep(URL_SLEEP)
+            else:
+                raise
+
+    return res
+
+
+def run():
+    """ Run the check.
+    """
+    try:
+        with open(LINKS_PATH, 'r') as fobj:
+            data = json.load(fobj)
+    except Exception:
+        message = 'No links found'
+        logging.warning(message)
+        create_mail(WARNING_SUBJECT_TEMPLATE.format(message), '')
+        return
+
+    session = requests.Session()
+    session.cookies.update(COOKIES)
+
+    for deck in data:
+        logging.info('Processing %s', deck)
+        content = get_url(session, data[deck]['url'])
+        match = re.search(ID_REGEX, content)
+        if not match:
+            raise ResponseError('No ID found')
+
+        deck_id = match.groups()[0]
+        if deck_id != data[deck]['id']:
+            message = 'Deck {} has been changed!'.format(deck)
+            logging.info(message)
+            send_discord(message)
+            create_mail(ALERT_SUBJECT_TEMPLATE.format(message))
+            data[deck]['id'] = deck_id
+            with open(LINKS_PATH, 'w') as fobj:
+                json.dump(data, fobj, indent=4)
 
 def main():
     """ Main function.
     """
-    cron_id = uuid.uuid4()
-    logging.info('Started: %s', cron_id)
+    logging.info('Started')
     try:
         if not internet_state():
             logging.warning('Internet is not available right now, exiting')
             return
 
-        last_message = get_sanity_check_message()
-        imported_main()
-        rclone()
-        if last_message:
-            message = 'Sanity check passed'
-            try:
-                if send_discord(message):
-                    set_sanity_check_message('')
-                    create_mail(SANITY_CHECK_SUBJECT_TEMPLATE.format(message))
-            except Exception as exc_new:
-                logging.exception(str(exc_new))
-    except SanityCheckError as exc:
-        message = str(exc)
+        run()
+    except ResponseError as exc:
+        message = 'Response Error: {}'.format(str(exc))
         logging.error(message)
-        if message != last_message:
-            try:
-                if send_discord(message):
-                    set_sanity_check_message(message)
-                    create_mail(SANITY_CHECK_SUBJECT_TEMPLATE.format(message))
-            except Exception as exc_new:
-                logging.exception(str(exc_new))
+        try:
+            create_mail(ERROR_SUBJECT_TEMPLATE.format(message))
+        except Exception as exc_new:
+            logging.exception(str(exc_new))
     except Exception as exc:
         message = 'Script failed: {}: {}'.format(type(exc).__name__, str(exc))
         logging.exception(message)
         try:
-            send_discord(message)
             create_mail(ERROR_SUBJECT_TEMPLATE.format(message))
         except Exception as exc_new:
             logging.exception(str(exc_new))
     finally:
-        logging.info('Finished: %s', cron_id)
-        logging.info('')
-        logging.info('')
+        logging.info('Finished')
 
 
 if __name__ == '__main__':
