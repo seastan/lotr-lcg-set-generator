2e3983b653be4305258ec6175fd4b448e2cfa3f5
Oleksandr Berchenko <ar.ukraine@gmail.com>
2021-05-29 19:58:33 +0300

    Add logging to run_all.bat

--- run_all.bat
+++ run_all.bat
@@ -4,6 +4,6 @@ python save_remote_logs_path.py
 for /f %%i in (remote_logs_path) do set log_path=%%i
 echo log path: %log_path%\run_all.log
 
-echo %date% %time% batch process started > %log_path%\run_all.log
+echo %date% %time% batch process started >> %log_path%\run_all.log
 call run_all_internal.bat >> %log_path%\run_all.log
 echo %date% %time% batch process finished >> %log_path%\run_all.log
