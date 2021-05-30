caf52a8d80718ecda871fafdd0c8b3c65b2bedee
Oleksandr Berchenko <ar.ukraine@gmail.com>
2021-05-30 18:10:32 +0300

    Fix run_all.bat

--- run_all_internal.bat
+++ run_all_internal.bat
@@ -12,25 +12,24 @@ python run_before_se_remote.py
 echo %date% %time% finished run_before_se_remote.py
 
 if not exist setGenerator_CREATED (
-  echo %date% %time% No Strange Eons project created, exiting
-  exit /b
-)
+  echo %date% %time% No Strange Eons project created
+) else (
+  echo %date% %time% started strange_eons.ahk
+  call strange_eons.ahk
+  echo %date% %time% finished strange_eons.ahk
 
-echo %date% %time% started strange_eons.ahk
-call strange_eons.ahk
-echo %date% %time% finished strange_eons.ahk
+  if not exist makeCards_FINISHED (
+    echo %date% %time% makeCards script didn't finish successfully, exiting
+    exit /b
+  )
 
-if not exist makeCards_FINISHED (
-  echo %date% %time% makeCards script didn't finish successfully, exiting
-  exit /b
-)
-
-echo %date% %time% waiting until Strange Eons is closed
-:loop
-timeout /t 5
-tasklist /fi "ImageName eq strangeeons.exe" /fo csv 2>NUL | find /I "strangeeons.exe">NUL
-if "%ERRORLEVEL%"=="0" (
-  goto loop
+  echo %date% %time% waiting until Strange Eons is closed
+  :loop
+  timeout /t 5
+  tasklist /fi "ImageName eq strangeeons.exe" /fo csv 2>NUL | find /I "strangeeons.exe">NUL
+  if "%ERRORLEVEL%"=="0" (
+    goto loop
+  )
 )
 
 echo %date% %time% started run_after_se_remote.py
