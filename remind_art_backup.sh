b4d07568b3469504eeecb24515bdbcf79553a48b
Oleksandr berchenko <ar.ukraine@gmail.com>
2021-06-30 18:12:36 +0300

    Rename remind_backup.sh

--- remind_art_backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Setup a cron as:
-# 0 8 * * 1   <path>/remind_art_backup.sh >> /home/homeassistant/.homeassistant/cron.log 2>&1
-cd "$(dirname "$0")"
-python3 create_mail.py "It's a good time to backup ALeP CardImages folder" "" > /dev/null
