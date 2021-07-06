dbd1529d2e2dbc1bd2300d4d3a4d971f00d0c08d
Oleksandr berchenko <ar.ukraine@gmail.com>
2021-07-06 18:41:28 +0300

    Fix the name of rclone remote

--- rclone_discord.sh
+++ rclone_discord.sh
@@ -2,4 +2,4 @@
 # Setup a cron as:
 # 27 * * * *  <path>/rclone_discord.sh >> <path>/cron.log 2>&1
 cd "$(dirname "$0")"
-rclone copy "Discord" "ALeP:/Discord"
+rclone copy "Discord" "ALePOCTGN:/Discord"
