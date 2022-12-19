78dccaf26f5662935887a589454f2d7bf2bf8fd2
Oleksandr berchenko <ar.ukraine@gmail.com>
2022-12-19 21:46:40 +0200

    Remove hardcoded dragncards hostname

--- remote_player_cards_stat_monitor.sh
+++ remote_player_cards_stat_monitor.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-LOG=`ssh webhost@dragncards.com '/home/webhost/python/AR/player_cards_stat_monitor.sh'`
+HOSTNAME=`cat discord.yaml | grep dragncards_hostname | awk '{print $2}'`
+LOG=`ssh $HOSTNAME '/home/webhost/python/AR/player_cards_stat_monitor.sh'`
 if [ ! "$LOG" ]; then
   python3 create_mail.py "No player cards stat cron logs for today" "" > /dev/null
 fi
