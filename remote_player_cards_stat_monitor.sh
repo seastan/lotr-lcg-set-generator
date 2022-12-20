3f84ff51a6ef277ec6c5e934f4b35cff6619c383
Oleksandr berchenko <ar.ukraine@gmail.com>
2022-12-20 16:12:04 +0200

    Provide explicit path to id_rsa

--- remote_player_cards_stat_monitor.sh
+++ remote_player_cards_stat_monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 HOSTNAME=`cat discord.yaml | grep dragncards_hostname | awk '{print $2}'`
-LOG=`ssh $HOSTNAME '/home/webhost/python/AR/player_cards_stat_monitor.sh'`
+LOG=`ssh $HOSTNAME -i id_rsa '/home/webhost/python/AR/player_cards_stat_monitor.sh'`
 if [ ! "$LOG" ]; then
   python3 create_mail.py "No player cards stat cron logs for today" "" > /dev/null
 fi
