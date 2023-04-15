d7e9d22db9b820c789b63d8401126a830ce78f72
Oleksandr berchenko <ar.ukraine@gmail.com>
2023-04-15 19:34:27 +0300

    Update monthly reminder

--- remind_stat_monthly.sh
+++ remind_stat_monthly.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 ./env_test.sh || exit
-python3 create_mail.py "It's a good time to collect RingsDB and DragnCards statistics for the previous month and make a backup" "" > /dev/null
+python3 create_mail.py "It's a good time to: 1. Collect RingsDB/DragnCards statistics and make a backup; 2. Check (1) issues on Google Drive" "" > /dev/null
