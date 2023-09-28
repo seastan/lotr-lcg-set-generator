6ff17a40bcd0232eac20c1f49e7d48b3e4e621bb
Oleksandr berchenko <ar.ukraine@gmail.com>
2023-09-28 15:41:22 +0300

    run UTC timestamp on any environment

--- utc_timestamp.sh
+++ utc_timestamp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-./env_test.sh || exit
+#./env_test.sh || exit
 date -u > Data/utc_timestamp.txt
 rclone copy "Data/utc_timestamp.txt" "ALePCardImages:/"
 rclone copy "Data/utc_timestamp.txt" "ALePStableData:/"
