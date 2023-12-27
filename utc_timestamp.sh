ed59eea3f754fc763da4ace57804c401be7caa47
Oleksandr berchenko <ar.ukraine@gmail.com>
2023-12-27 20:07:37 +0200

    Fix date format

--- utc_timestamp.sh
+++ utc_timestamp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 #./env_test.sh || exit
-date -u > Data/utc_timestamp.txt
+date -u '+%a %d %b %H:%M:%S %Z %Y' > Data/utc_timestamp.txt
 rclone copy "Data/utc_timestamp.txt" "ALePCardImages:/"
 rclone copy "Data/utc_timestamp.txt" "ALePStableData:/"
