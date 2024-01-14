dca5ea34e97a326edba3374d18ad8cc5a3359c5f
Oleksandr berchenko <ar.ukraine@gmail.com>
2024-01-14 17:12:38 +0200

    Display an explicit error when no folder was specified

--- scheduled_backup.sh
+++ scheduled_backup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 ./env_test.sh || exit
+if [ ! "$1" ]; then
+  echo "Error: No folder specified"
+  exit 1
+fi
 python3 scheduled_backup.py "$1"
 rclone copy "$1" "ALePLinksBackup:/"
