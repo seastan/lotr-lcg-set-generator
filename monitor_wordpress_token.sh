50d8dea24db430c4b1fd51c598e49692f2d4eae8
Oleksandr berchenko <ar.ukraine@gmail.com>
2024-03-03 14:16:03 +0200

    Update error message

--- monitor_wordpress_token.sh
+++ monitor_wordpress_token.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$0")"
 TOKEN=`grep -a wordpress_token mpc_monitor.json | sed -e 's/^\s* "wordpress_token": "//' -e 's/",$//'`
 RES=`curl -G -s --data-urlencode "client_id=76623" --data-urlencode "token=$TOKEN" https://public-api.wordpress.com/oauth2/token-info | grep -a '"client_id":"76623"'`
 if [[ ! "$RES" ]]; then
-  python3 create_mail.py "Wordpress token expired" "" > /dev/null
+  python3 create_mail.py "Wordpress token might be expired: can't obtain the correct response from the server" "" > /dev/null
 fi
